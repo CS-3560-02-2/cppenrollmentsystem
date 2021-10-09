a6af27ac73ed3923a2e81b57c3c86b4d22219646
Daniel Dinh <dinhdaniel10153@gmail.com>
2021-10-08 20:14:45 -0700

    class course skeleton

---
+++ Course.h
@@ -0,0 +1,16 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+class Course
+{
+public:
+    Course();
+    
+private:
+    int courseNumber;
+    string courseTitle;
+    int courseUnits;
+    string coursePrereq;
+};
+
+#endif
