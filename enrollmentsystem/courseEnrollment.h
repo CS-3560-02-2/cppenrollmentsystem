672b57ea44d6fb67be41f054203d8634587dbdcc
Cameron Larsen <camjlarsen@gmail.com>
2021-10-08 21:55:11 -0700

    Added comments to courseEnrollment.h

--- enrollmentsystem/courseEnrollment.h
+++ enrollmentsystem/courseEnrollment.h
@@ -1,13 +1,14 @@
 // courseEnrollment.h
 
+// Association class between Student and courseSection 
 namespace courseEnrollment
 {
-    char Grade;
+    char Grade;     // holds letter grade for student
 
 
     class courseEnrollment
     {
-        
+        courseEnrollment(); // constructor
     }
 }
 
