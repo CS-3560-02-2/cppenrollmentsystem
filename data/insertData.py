fa1848c7be486bf11909fc066dbf63f0997e5fc2
Daniel Dinh <dinhdaniel10153@gmail.com>
2021-11-10 21:34:31 -0800

    Data with students and instructor, still need course, course_sections, course_enrollment data

--- data/insertData.py
+++ data/insertData.py
@@ -1,27 +1,29 @@
 import sqlite3
 
-conn = sqlite3.connect(":memory") #what to connect to
+conn = sqlite3.connect(":memory:") #what to connect to, memory just put a fresh one in ram so it won't need to worry about duplications
 cursor = conn.cursor() #used to query
-#""" doc string
 
-# cursor.execute("INSERT INTO courses VALUES (1, 'Database', 'learningDatabases', 3, 'CS', 2000 )")
-cursor.execute(
-    """INSERT INTO students (first_name,last_name,street,city,state,zip_code,phone_number,email,major) VALUES
-    ("Rigel","Morales","308-3017 Justo Rd.","Morwell","CT","415018","(712) 592-7514","dis.parturient@protonmail.edu","fermentum"),
-    ("Ann","Bolton","6166 Felis St.","Wolfsberg","AK","352627","(606) 582-8587","ut.tincidunt@icloud.edu","mollis"),
-    ("Merritt","Foreman","P.O. Box 437, 7411 Etiam St.","Linköping","MD","43281-322","1-453-968-3344","luctus.et@aol.couk","a"),
-    ("Basia","Levine","P.O. Box 833, 3593 Mauris St.","Jeju","WY","594586","(372) 355-2365","scelerisque@outlook.com","risus."),
-    ("Richard","Leon","Ap #602-8550 Suscipit, Road","La Dorada","UT","9260","1-357-550-4043","sagittis.duis@icloud.edu","posuere,")"""
-)
-conn.commit()
+def insert_students():
+    cursor.execute(
+        """INSERT INTO students (first_name,last_name,street,city,state,zip_code,phone_number,email,major) VALUES
+        ("Rigel","Morales","308-3017 Justo Rd.","Morwell","CT","415018","(712) 592-7514","dis.parturient@protonmail.edu","fermentum"),
+        ("Ann","Bolton","6166 Felis St.","Wolfsberg","AK","352627","(606) 582-8587","ut.tincidunt@icloud.edu","mollis"),
+        ("Merritt","Foreman","P.O. Box 437, 7411 Etiam St.","Linköping","MD","43281-322","1-453-968-3344","luctus.et@aol.couk","a"),
+        ("Basia","Levine","P.O. Box 833, 3593 Mauris St.","Jeju","WY","594586","(372) 355-2365","scelerisque@outlook.com","risus."),
+        ("Richard","Leon","Ap #602-8550 Suscipit, Road","La Dorada","UT","9260","1-357-550-4043","sagittis.duis@icloud.edu","posuere,")"""
+    )
+    conn.commit()
 
-cursor.execute("INSERT INTO courses VALUES ('Database', 'learningDatabases', 3, 'CS', 2000 )")
-        # cursor.execute("SELECT * FROM students WHERE last_name='Morales'")
-        # print(cursor.fetchone())
-        # cursor.execute("SELECT * FROM courses WHERE course_title='Database'")
-        # print(cursor.fetchone())
-conn.commit()
-
+def insert_instructors():
+    cursor.execute(
+        """INSERT INTO instructors (first_name,last_name,street,city,state,zip_code,phone_number,email,major) VALUES
+        ("Lavinia","Haney","1354 Vitae Ave","HŽron","WA","71836","1-785-724-4184","lacinia@icloud.net","Engineering,"),
+        ("Noah","Suarez","569-7471 Sed Street","San Juan del Río","LA","36049","1-396-743-1972","placerat@google.org","Liberal Arts"),
+        ("Asher","Perry","5034 Scelerisque, Rd.","Sclayn","MD","121185","1-682-786-5048","sagittis.semper@yahoo.org","Science"),
+        ("Rama","Kramer","Ap #263-4014 Dignissim Street","Łomża","WY","50628","1-651-228-8212","curabitur.sed@protonmail.org","Math,"),
+        ("Rebecca","Cantu","5277 Placerat St.","Woodstock","LA","1085","1-311-158-3438","vulputate.ullamcorper@hotmail.com","Technology")"""
+    )
+    conn.commit()
 
 
 conn.close()
