17d32c61722250b7210f6934b715522707e74655
Daniel Dinh <dinhdaniel10153@gmail.com>
2021-11-11 22:20:18 -0800

    added more search functions

--- data/selectData.py
+++ data/selectData.py
@@ -16,4 +16,25 @@ def search_instructor(email):
         cursor.execute("SELECT * FROM instructor WHERE email=?", (email,))
         return cursor.fetchone()
 
+def search_course_count():
+    with conn:
+        cursor.execute("SELECT COUNT (*) FROM courses") #count to return the number of courses
+        return cursor.fetchone()
+
+def search_all_courses(subject):
+    with conn:
+        cursor.execute("SELECT COUNT (*) FROM courses WHERE subject=?", (subject,))
+        return cursor.fetchone()
+
+def select_course(subject, course_num):
+    with conn:
+        cursor.execute("SELECT * FROM courses WHERE subject=?, course_num=?", (subject, course_num,))
+        return cursor.fetchone()
+
+def select_all_sections(course_id):
+    with conn:
+        cursor.execute("SELECT * FROM course_sections WHERE subject=?, course_id=?", (course_id,))
+        return cursor.fetchone()
+#havent checked yet
+
 conn.close()
